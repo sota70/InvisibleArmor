90156d5a21e8c05a0af35d97620a50f7d6bccb09
sota70 <sota52350@gmail.com>
2021-05-05 15:09:37 +0900

    Add dokka comments

--- src/main/java/com/invisiblearmor/armordata/PlayerArmorInitializer.kt
+++ src/main/java/com/invisiblearmor/armordata/PlayerArmorInitializer.kt
@@ -3,17 +3,28 @@ package com.invisiblearmor.armordata
 import com.invisiblearmor.InvisibleArmor
 import org.bukkit.entity.Player
 
+/**
+ * プレイヤーのアーマーデータを初期化するクラス
+ *
+ * @property plugin プラグインのメインクラス
+ * @property player 初期化する対象のプレイヤー
+ */
 class PlayerArmorInitializer(
     private val plugin: InvisibleArmor,
     private val player: Player
 ) {
 
+    /**
+     * プレイヤーのデータを初期化するメソッド
+     * ToDo: コンフィグにアーマーデータを保存して、初期化時に呼び出す処理を作る
+     */
     fun initPlayerArmorData() {
         if (isPlayerAlreadyInitialized()) return
         val playerArmorData = plugin.getPlayerArmorData()
         playerArmorData[player] = PlayerArmor()
     }
 
+    // すでにプレイヤーのアーマーデータが初期化されているか確認するメソッド
     private fun isPlayerAlreadyInitialized(): Boolean {
         val playerArmorData = plugin.getPlayerArmorData()
         return playerArmorData.containsKey(player)
