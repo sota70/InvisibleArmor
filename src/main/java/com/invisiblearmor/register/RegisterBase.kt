641d17f4c681cc77d51b2119915e4548c82b595a
sota70 <sota52350@gmail.com>
2021-05-05 15:13:05 +0900

    Add dokka comments

--- src/main/java/com/invisiblearmor/register/RegisterBase.kt
+++ src/main/java/com/invisiblearmor/register/RegisterBase.kt
@@ -2,9 +2,18 @@ package com.invisiblearmor.register
 
 import com.invisiblearmor.InvisibleArmor
 
+/**
+ * Registerクラスのベースとなるインターフェース
+ */
 interface RegisterBase {
 
+    /**
+     * プラグインのメインクラス
+     */
     val plugin: InvisibleArmor
 
+    /**
+     * 何かを登録するメソッド
+     */
     fun register()
 }
--- src/main/java/com/invisiblearmor/template/ArmorGUIName.kt
+++ src/main/java/com/invisiblearmor/template/ArmorGUIName.kt
@@ -1,6 +1,12 @@
 package com.invisiblearmor.template
 
+/**
+ * ArmorGUIの名前を保管しておくオブジェクト
+ */
 object ArmorGUIName {
 
+    /**
+     * ArmorGUIの名前
+     */
     val ARMOR_GUI_NAME = "Armor GUI"
 }
