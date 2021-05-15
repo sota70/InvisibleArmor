bc947610409973ddbb9d14a70fcd14e648f1164e
sota70 <sota52350@gmail.com>
2021-05-15 17:51:27 +0900

    make 2 private methods to make code clear

--- src/main/java/com/invisiblearmor/listener/TakeDamageListener.kt
+++ src/main/java/com/invisiblearmor/listener/TakeDamageListener.kt
@@ -1,11 +1,13 @@
 package com.invisiblearmor.listener
 
 import com.invisiblearmor.InvisibleArmor
-import com.invisiblearmor.damagecalculator.DamageCalculator
+import com.invisiblearmor.damagecalculator.DamageCalculatorFactory
+import com.invisiblearmor.playerstatus.StatusUpdaterFactory
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause
 
 /**
  * ダメージを受けたことを感知するリスナークラス
@@ -23,11 +25,30 @@ class TakeDamageListener(private val plugin: InvisibleArmor) : Listener {
         if (event.entity !is Player) return
         val player = event.entity as Player
         val takenDamage = event.finalDamage
-        val finalDamage = DamageCalculator(plugin, player, takenDamage).calcFinalDamage()
+        val finalDamage = calcFinalDamage(player, takenDamage, event.cause)
         event.damage = finalDamage
+        updatePlayerStatus(player, event.cause)
 
         // デバッグ用
         println("Taken Damage: $takenDamage")
         println("Final Damage: $finalDamage")
+    }
+
+    private fun calcFinalDamage(
+        player: Player,
+        takenDamage: Double,
+        damageType: DamageCause
+    ): Double {
+        val damageCalculator =
+            DamageCalculatorFactory(plugin, player, takenDamage).createCalculator(damageType)
+        return damageCalculator.calcFinalDamage()
+    }
+
+    private fun updatePlayerStatus(
+        player: Player,
+        damageType: DamageCause
+    ) {
+        val playerStatusUpdater = StatusUpdaterFactory(plugin).createStatusUpdater(damageType)
+        playerStatusUpdater.updateStatus(player)
     }
 }
