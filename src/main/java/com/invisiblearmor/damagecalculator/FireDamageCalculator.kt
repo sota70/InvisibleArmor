6e63bcde46478dabf737a754ea70390bffaf1051
sota70 <sota52350@gmail.com>
2021-06-04 17:01:47 +0900

    Add documentation comments

--- src/main/java/com/invisiblearmor/damagecalculator/FireDamageCalculator.kt
+++ src/main/java/com/invisiblearmor/damagecalculator/FireDamageCalculator.kt
@@ -4,12 +4,24 @@ import com.invisiblearmor.InvisibleArmor
 import com.invisiblearmor.armordata.armorloader.PlayerFireProtectionLoader
 import org.bukkit.entity.Player
 
+/**
+ * 炎ダメージの計算クラス
+ *
+ * @property plugin プラグインのメインクラス
+ * @property player ダメージを受けたプレイヤー
+ * @property damage 受けたダメージ
+ */
 class FireDamageCalculator(
     private val plugin: InvisibleArmor,
     private val player: Player,
     private val damage: Double
 ) : DamageCalculator {
 
+    /**
+     * プレイヤーのFire Protection値からダメージを計算するメソッド
+     *
+     * @return 計算したダメージを[Double]型で返す
+     */
     override fun calcFinalDamage(): Double {
         val playerFireProtection =
             PlayerFireProtectionLoader(plugin, player).fetchPlayerFireProtection()
