a2256c1a93f59178ffe4528b0c736f6d76d16527
sota70 <sota52350@gmail.com>
2021-06-04 16:59:42 +0900

    Add documentation comments

--- src/main/java/com/invisiblearmor/announcement/Announce.kt
+++ src/main/java/com/invisiblearmor/announcement/Announce.kt
@@ -2,9 +2,24 @@ package com.invisiblearmor.announcement
 
 import org.bukkit.entity.Player
 
+/**
+ * Announceクラスのベースクラス
+ * メッセージを送信するときに使う
+ */
 interface Announce {
 
+    /**
+     * メッセージを送信する対象のプレイヤー
+     */
     val player: Player
 
-    fun announce()
+    /**
+     * メッセージを送信するメソッド (単体メッセージ）
+     */
+    fun announce(announceMessage: String)
+
+    /**
+     * メッセージを送信するメソッド (複数メッセージ）
+     */
+    fun announce(announceMessages: Array<String>)
 }
