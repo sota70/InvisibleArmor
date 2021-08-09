d0b378d3a105b5a71a28e2a3cf587e55db2af1b4
sota70 <sota52350@gmail.com>
2021-08-09 15:03:28 +0900

    Add some listeners

--- src/main/java/com/neoinvisiblearmor/registerer/ListenerRegisterer.kt
+++ src/main/java/com/neoinvisiblearmor/registerer/ListenerRegisterer.kt
@@ -1,10 +1,7 @@
 package com.neoinvisiblearmor.registerer
 
 import com.neoinvisiblearmor.NeoInvisibleArmor
-import com.neoinvisiblearmor.listener.CloseArmorGUIListener
-import com.neoinvisiblearmor.listener.EditArmorGUIListener
-import com.neoinvisiblearmor.listener.JoinListener
-import com.neoinvisiblearmor.listener.TakeDamageListener
+import com.neoinvisiblearmor.listener.* // ktlint-disable no-wildcard-imports
 import org.bukkit.Bukkit
 
 /**
@@ -23,6 +20,17 @@ class ListenerRegisterer(
         Bukkit.getPluginManager().registerEvents(CloseArmorGUIListener(plugin), plugin)
         Bukkit.getPluginManager().registerEvents(EditArmorGUIListener(), plugin)
         Bukkit.getPluginManager().registerEvents(JoinListener(plugin), plugin)
-        Bukkit.getPluginManager().registerEvents(TakeDamageListener(plugin), plugin)
+        Bukkit.getPluginManager().registerEvents(PlayerTakeDamageListener(plugin), plugin)
+        Bukkit.getPluginManager().registerEvents(TakeStatusDamageListener(plugin), plugin)
+        Bukkit.getPluginManager().registerEvents(PlayerBurnListener(plugin), plugin)
+        Bukkit.getPluginManager().registerEvents(PlayerTakeDamageByEntityListener(plugin), plugin)
+        Bukkit.getPluginManager().registerEvents(PlayerTakeDamageByAreaEffectCloudListener(plugin), plugin)
+
+        /*
+         爆発時の最大爆発軽減値があまりに低いため、爆発軽減機能は省くことにした
+         バニラのエンチャントだとほぼ０に等しい量しか軽減されていない
+
+         Bukkit.getPluginManager().registerEvents(PlayerExposedExplosionListener(), plugin)
+         */
     }
 }
