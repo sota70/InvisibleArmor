740f6b021cd3ca99a6220c8d0d6d9b27293c6568
sota70 <sota52350@gmail.com>
2021-08-09 15:06:56 +0900

    Add falling anvil reduction calculation

--- src/main/java/com/neoinvisiblearmor/util/DamageCalculator.kt
+++ src/main/java/com/neoinvisiblearmor/util/DamageCalculator.kt
@@ -1,5 +1,8 @@
 package com.neoinvisiblearmor.util
 
+import org.bukkit.Material
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause
+
 /**
  * ダメージを計算するクラス
  *
@@ -8,32 +11,77 @@ package com.neoinvisiblearmor.util
  * @property armorEPF EPFポイント
  * @property armorToughness ArmorToughness（ダイヤ・ネザライト装備についてるやつ）
  */
-class DamageCalculator(
-    private val takenDamage: Double,
-    private val armorPoint: Double,
-    private val armorEPF: Double,
-    private val armorToughness: Double
-) {
+class DamageCalculator {
 
-    /**
-     * [armorPoint]、[armorEPF]をもとにダメージを計算するメソッド
-     *
-     * @return 計算したダメージを[Double]型で返す
-     */
-    fun calcFinalDamageWithEPF(): Double {
-        val reduction =
-            (100 - (armorPoint * 4)) * armorEPF * 0.04 * 0.0075
-        return takenDamage * reduction
+    private var takenDamage = 0.0
+    private var armorPoint = 0.0
+    private var armorEPF = 0.0
+    private var armorToughness = 0.0
+    private var damageType = DamageCause.VOID
+    private var itemOnPlayerHead = Material.AIR
+
+    constructor()
+
+    constructor(
+        _takenDamage: Double,
+        _armorPoint: Double,
+        _armorEPF: Double,
+        _armorToughness: Double,
+        _damageType: DamageCause,
+        _itemOnPlayerHead: Material
+    ) {
+        takenDamage = _takenDamage
+        armorPoint = _armorPoint
+        armorEPF = _armorEPF
+        armorToughness = _armorToughness
+        damageType = _damageType
+        itemOnPlayerHead = _itemOnPlayerHead
     }
 
     /**
-     * [armorPoint]をもとにダメージを計算するメソッド
+     * [armorPoint], [armorToughness], [armorEPF]をもとにダメージを計算して値を返すメソッド
+     * [計算式引用元][https://minecraft.fandom.com/wiki/Armor#Damage_protection]
+     * DamageCauseがFalling Blockかつ、プレイヤーがヘルメットを装備している場合に
+     * ダメージを25%軽減させる
      *
-     * @return 計算したダメージを[Double]型で返す
+     * @return 最終ダメージを[Double]型で返す
      */
-    fun calcFinalDamageWithoutEPF(): Double {
-        val reduction =
-            1 - (Math.min(20.0, Math.max(armorPoint / 5, armorPoint - (takenDamage * 4 / (armorToughness + 8))))) / 25
-        return takenDamage * reduction
+    fun calcFinalDamage(): Double {
+        sendArmorLog()
+        val helmetReduction = 0.25
+        val finalDamage =
+            takenDamage * (1 - Math.min(20.0, Math.max(armorPoint / 5, armorPoint - takenDamage / (2 + armorToughness / 4))) / 25) * (1 - Math.min(armorEPF, 20.0) / 25)
+        if (damageType == DamageCause.FALLING_BLOCK && itemOnPlayerHead != Material.AIR) {
+            return finalDamage * (1 - helmetReduction)
+        }
+        return finalDamage
+    }
+
+    fun setTakenDamage(_takenDamage: Double): DamageCalculator =
+        DamageCalculator(_takenDamage, armorPoint, armorEPF, armorToughness, damageType, itemOnPlayerHead)
+
+    fun setArmorPoint(_armorPoint: Double): DamageCalculator =
+        DamageCalculator(takenDamage, _armorPoint, armorEPF, armorToughness, damageType, itemOnPlayerHead)
+
+    fun setArmorEPF(_armorEPF: Double): DamageCalculator =
+        DamageCalculator(takenDamage, armorPoint, _armorEPF, armorToughness, damageType, itemOnPlayerHead)
+
+    fun setArmorToughness(_armorToughness: Double): DamageCalculator =
+        DamageCalculator(takenDamage, armorPoint, armorEPF, _armorToughness, damageType, itemOnPlayerHead)
+
+    fun setDamageType(_damageType: DamageCause): DamageCalculator =
+        DamageCalculator(takenDamage, armorPoint, armorEPF, armorToughness, _damageType, itemOnPlayerHead)
+
+    fun setItemOnPlayerHead(_itemOnPlayerHead: Material): DamageCalculator =
+        DamageCalculator(takenDamage, armorPoint, armorEPF, armorToughness, damageType, _itemOnPlayerHead)
+
+    private fun sendArmorLog() {
+        println(
+            """
+            ArmorPoint: $armorPoint
+            ArmorToughness: $armorToughness
+            ArmorEPF: $armorEPF
+            """.trimIndent()
+        )
     }
 }
