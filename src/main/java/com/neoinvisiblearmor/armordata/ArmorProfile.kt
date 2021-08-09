b3b97fc4ecd36cb4cbf9ff7f7abdb8a92cf7e9c4
sota70 <sota52350@gmail.com>
2021-08-09 14:46:24 +0900

    Add ArmorProfile

---
+++ src/main/java/com/neoinvisiblearmor/armordata/ArmorProfile.kt
@@ -0,0 +1,12 @@
+package com.neoinvisiblearmor.armordata
+
+import org.bukkit.enchantments.Enchantment
+
+val STACKABLE_ARMOR_ENCHANTMENTS = arrayOf(
+    Enchantment.PROTECTION_ENVIRONMENTAL,
+    Enchantment.PROTECTION_EXPLOSIONS,
+    Enchantment.PROTECTION_PROJECTILE,
+    Enchantment.OXYGEN,
+    Enchantment.WATER_WORKER,
+    Enchantment.THORNS
+)
